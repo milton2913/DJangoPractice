73efc807e677f09e2d765a387db1e548e678ab94
Milton <milton2913@gmail.com>
2025-02-19 01:23:19 +0600

    app registration ad app class registration

--- first/welcome/admin.py
+++ first/welcome/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from welcome.models import pages
 # Register your models here.
+class pagesAdmin(admin.ModelAdmin):
+    list_display = ('id','title','description')
+admin.site.register(pages,pagesAdmin)
--- first/welcome/models.py
+++ first/welcome/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class pages(models.Model):
+    id=models.IntegerField(primary_key=True)
+    title=models.CharField(max_length=150)
+    description=models.TextField()
